9921baf2bde8eee8c82fa085983a7c4885ba64b7
Jason Chiang <jason.chg@outlook.com>
2020-01-07 18:03:37 +0800

    add counter to shopping cart

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -8,11 +8,16 @@ import CartItem from "../cart-item/CartItem";
 import { connect } from "react-redux";
 
 const CartDropdown = ({ cartItems }) => {
+  var counter = 0;
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
         {cartItems.map(cartItem => (
-          <CartItem key={cartItem.id} item={cartItem} />
+          <CartItem
+            key={cartItem.id}
+            item={cartItem}
+            counter={(counter += 1)}
+          />
         ))}
       </div>
 
--- src/components/cart-item/CartItem.js
+++ src/components/cart-item/CartItem.js
@@ -3,10 +3,12 @@ import React from "react";
 import "./CartItem.styles.scss";
 
 export default function CartItem({
+  counter,
   item: { imageUrl, price, name, quantity }
 }) {
   return (
     <div className="cart-item">
+      <span>{counter}.</span>
       <img src={imageUrl} alt="item" />
       <div className="item-details">
         <span className="name">{name}</span>
